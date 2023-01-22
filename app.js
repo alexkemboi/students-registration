989a3a554a887bea67f540a59833e7cdc33cb63d
alexkemboi4127 <alexkemboi97@yahoo.com>
2023-01-23 00:53:50 +0300

    added bootstrap,worked on delete,edit and search button

--- app.js
+++ app.js
@@ -1,128 +1,227 @@
-let BASE_URL = 'http://localhost:3000';
+let BASE_URL = "http://localhost:3000";
 const btn = document.querySelector("#submitbtn");
-let tableDiv = document.getElementById('tableData');
-btn.addEventListener('click', (e) => {
-	let id = document.querySelector("#id");
-	let firstname = document.querySelector("#firstname");
-	let lastname = document.querySelector("#lastname");
-	let age = document.querySelector("#age");
-	let dob = document.querySelector("#dob");
-	e.preventDefault();
-	let data = {
-		id: id.value,
-		firstname: firstname.value,
-		lastname: lastname.value,
-		age: age.value,
-		dob: dob.value,
-	};
+let tableDiv = document.getElementById("tableData");
+let formMessage = document.getElementById("formMessage");
+let id = document.querySelector("#id");
+let firstname = document.querySelector("#firstname");
+let lastname = document.querySelector("#lastname");
+let age = document.querySelector("#age");
+let dob = document.querySelector("#dob");
+let countries = document.querySelector("#countries");
+let gender = document.querySelector("#gender");
 
-	postData(data);
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let data = {
+    id: id.value,
+    firstname: firstname.value,
+    lastname: lastname.value,
+    age: age.value,
+    dob: dob.value,
+    countries: countries.value,
+    gender: gender.value,
+  };
 
+  postData(data);
+});
 
-})
-
-
-function postData(data){
-	try {
-		fetch(`${BASE_URL}`, {
-			method:'POST',
-			body: JSON.stringify(data),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		}).then(res => res.json()).then(data => {
-			alert('Data inserted successfully');
-			console.log({status: "success", data: JSON.stringify(data)});
-			getDataAndShowInTable()
-		})
-	} catch (err) {
-		console.log(err)
-	}
-
-	
+function postData(data) {
+  console.log(data);
+  try {
+    fetch(`${BASE_URL}`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        let html = "";
+        html += `<p class="text-success">Data inserted successfully!</p>`;
+        formMessage.innerHTML = html;
+        console.log({ status: "success", data: JSON.stringify(data) });
+        getDataAndShowInTable();
+      });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
+function getDataAndShowInTable() {
+  let result = fetch(`${BASE_URL}`)
+    .then((res) => res.json())
+    .then((data) => {
+      let currentPage = 1;
+      let pageSize = 5;
 
-function getDataAndShowInTable(){
-	let result = fetch(`${BASE_URL}`).then(res => res.json()).then(data => {
-		let html = '';
-		data.forEach(item => {
-			html += `<tr>
+      function displayPage(data, pageNumber, pageSize) {
+        let startIndex = (pageNumber - 1) * pageSize;
+        let endIndex = startIndex + pageSize;
+        let currentData = data.slice(startIndex, endIndex);
+
+        let html = "";
+        currentData.forEach((item) => {
+          html += `<tr>
 			<td>${item.id}</td>
 			<td>${item.firstname}</td>
 			<td>${item.lastname}</td>
 			<td>${item.age}</td>
 			<td>${item.dob}</td>
-			</tr>	`
-		})		
-		tableDiv.innerHTML = html;
-	})
+			<td>${item.countries}</td>			
+			<td>${item.gender}</td>
+			</tr>	`;
+        });
+        // insert the generated html into the appropriate element
+        tableDiv.innerHTML = html;
 
+        let totalPages = Math.ceil(data.length / pageSize);
+        for (let i = 1; i <= totalPages; i++) {
+          let button = document.createElement("button");
+          button.classList.add("btn-dark");
+          button.innerHTML = i;
+          button.addEventListener("click", () => {
+            currentPage = i;
+            displayPage(data, currentPage, pageSize);
+          });
+          tableDiv.appendChild(button);
+        }
+      }
+
+      displayPage(data, currentPage, pageSize);
+    });
 }
-
-
 
 function searchRecord() {
-	const searchValue=document.getElementById("search").value;
+  const searchInput = document.getElementById("search");
+  searchValue = searchInput.value;
+  console.log(searchValue);
+  let obj = {};
+  let key = "lastname";
 
-	console.log(searchValue);
-    // Use the fetch method to send a GET request to the server
-    fetch(`${BASE_URL}/search?searchTerm=${encodeURIComponent(searchValue)}`,{
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	})
-        .then(response => response.json())
-        .then(data => {
-            // Do something with the data
-			if(data.length>0){
-			let html = '';
-		data.forEach(item => {
-			html += `<tr>
+  // Use the fetch method to send a GET request to the server
+  fetch(`${BASE_URL}/search?searchTerm=${encodeURIComponent(searchValue)}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // Do something with the data
+      console.log(data);
+      if (data && data.length > 0) {
+        // do something with the data
+        let html = "";
+        data.forEach((item) => {
+          html += `<tr>
 			<td>${item.id}</td>
 			<td>${item.firstname}</td>
 			<td>${item.lastname}</td>
 			<td>${item.age}</td>
 			<td>${item.dob}</td>
-			</tr>	`
-		})		
-		tableDiv.innerHTML = html;
-            console.log(data);
-	}else{
-		let html = '';
-			html += `No similar record`;
-			tableDiv.innerHTML = html;
-	}
-        })
-        .catch(error => {
-            console.error(error);
+			<td>${item.countries}</td>			
+			<td>${item.gender}</td>
+			</tr>	`;
         });
+
+        tableDiv.innerHTML = html;
+        //populate form inputs with data
+        if (data && data.length > 0) {
+          // do something with the data
+          id.value = data[0].id;
+          firstname.value = data[0].firstname;
+          lastname.value = data[0].lastname;
+          age.value = data[0].age;
+          dob.value = data[0].dob;
+          countries.value = data[0].countries;
+          gender.value = data[0].gender;
+        }
+      } else {
+        id.value = "";
+        firstname.value = "";
+        lastname.value = "";
+        age.value = "";
+        dob.value = "";
+        countries.value = "";
+        gender.value = "";
+        console.log("No results found");
+        let html = "";
+        html += `<p class="text-danger text-centre">No  record found</p>`;
+        tableDiv.innerHTML = html;
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 function deleteSearchTerm() {
-    const searchValue=document.getElementById("search").value;
-
-    // Use the fetch method to send a DELETE request to the server
-    fetch(`${BASE_URL}/delete?searchTerm=${searchValue}`,{
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        }
+  const searchValue = document.getElementById("search").value;
+  // Use the fetch method to send a DELETE request to the server
+  fetch(`${BASE_URL}/delete?searchTerm=${searchValue}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        console.log("Search term deleted successfully!");
+        let html = "";
+        html += `<p class="text-success">Search term deleted successfully!</p>`;
+        tableDiv.innerHTML = html;
+      } else {
+        console.log("Error deleting search term: ", response.status);
+      }
     })
-    .then(response => {
-        if(response.status === 200) {
-            console.log("Search term deleted successfully!");
-			let html = '';
-			html += `Search term deleted successfully!`;
-			tableDiv.innerHTML = html;
-	
-        } else {
-            console.log("Error deleting search term: ", response.status);
-        }
-    })
-    .catch(error => {
-        console.error(error);
+    .catch((error) => {
+      console.error(error);
     });
 }
+
+const selectElement = document.getElementById("countries");
+fetch("https://restcountries.com/v2/all")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((country) => {
+      const option = document.createElement("option");
+      option.value = country.name;
+      option.innerHTML = country.name;
+      selectElement.appendChild(option);
+    });
+  });
+
+function updateRecord() {
+  let updateData = {
+    id: id.value,
+    firstname: firstname.value,
+    lastname: lastname.value,
+    age: age.value,
+    dob: dob.value,
+    countries: countries.value,
+    gender: gender.value,
+  };
+  try {
+    fetch(`${BASE_URL}/update`, {
+      method: "POST",
+      body: JSON.stringify(updateData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((updateData) => {
+        let html = "";
+        html += `<p class="text-success">Data updated successfully!</p>`;
+        formMessage.innerHTML = html;
+        console.log({ status: "success", data: JSON.stringify(updateData) });
+        getDataAndShowInTable();
+      });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 getDataAndShowInTable();
--- index.js
+++ index.js
@@ -1,129 +1,155 @@
 const express = require("express");
 const mysql = require("mysql");
-const cors = require('cors');
-
-
+const cors = require("cors");
 
 // Create connection
 
 const db = mysql.createConnection({
-    host: "localhost",    
-    user: "root",    
-    password: "",    
-    database: "student",
-    
-    });
-    // Connect to MySQL
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "student",
+});
+// Connect to MySQL
 
 db.connect((err) => {
-    if (err) {
-        throw err; 
-    }  console.log("MySql Connected");    
-    });
+  if (err) {
+    throw err;
+  }
+  console.log("MySql Connected");
+});
 const app = express();
 
 // enable cors
-app.use(cors({
-    origin: '*'
-})) 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use(express.json())
-
+app.use(express.json());
 
 // Create DB
 
 app.get("/createdb", (req, res) => {
-    let sql = "CREATE DATABASE nodemysql";
-    db.query(sql, (err) => {
+  let sql = "CREATE DATABASE nodemysql";
+  db.query(sql, (err) => {
     if (err) {
-    throw err;
+      throw err;
     }
     res.send("Database created");
-    });
-    });
+  });
+});
 
-    //delete record
-    app.get("/delete", (req, res) => {
-      let sql ="DELETE FROM `studentdetails` WHERE id=1";
-      db.query(sql, (err) => {
-          if (err) {
-              throw err;
-          }    
-      res.send("Data deleted successfully");
-      });    
-      });
-  
+//delete record
+app.get("/delete", (req, res) => {
+  let sql = "DELETE FROM `studentdetails` WHERE id=1";
+  db.query(sql, (err) => {
+    if (err) {
+      throw err;
+    }
+    res.send("Data deleted successfully");
+  });
+});
+
 // Create table
 
 app.get("/createemployee", (req, res) => {
-    let sql ="CREATE TABLE employee(id int AUTO_INCREMENT, name VARCHAR(255), designation VARCHAR(255), PRIMARY KEY(id))";
-    db.query(sql, (err) => {
-        if (err) {
-            throw err;
-        }    
+  let sql =
+    "CREATE TABLE employee(id int AUTO_INCREMENT, name VARCHAR(255), designation VARCHAR(255), PRIMARY KEY(id))";
+  db.query(sql, (err) => {
+    if (err) {
+      throw err;
+    }
     res.send("Employee table created");
-    });    
-    });
+  });
+});
 
 // Insert data
 
 app.post("/", (req, res) => {
   try {
-    const {id, firstname, lastname, age, dob} = req.body;
+    const { id, firstname, lastname, age, dob, countries, gender } = req.body;
 
-    db.query('INSERT INTO studentdetails (id, firstname, lastname, age, dob) values (?, ?, ?, ?, ?)', [id,firstname,lastname, age, dob ], function (error) {
+    db.query(
+      "INSERT INTO studentdetails (id, firstname, lastname, age, dob,countries,gender) values (?, ?, ?, ?, ?,?,?)",
+      [id, firstname, lastname, age, dob, countries, gender],
+      function (error) {
+        if (error) console.log(error);
 
-        if (error)  console.log(error); 
-       
-        res.status(200).send({message:'Data inserted successfully!'});
-       
-        }); 
-    
+        res.status(200).send({ message: "Data inserted successfully!" });
+      }
+    );
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 });
 
-
 // fetch all users
-app.get('/',(req, res)=> {  
-    db.query('SELECT * FROM studentdetails', (err,rows)=> {
-      if (err) {
-       console.log(err);
-      } else {
-        res.status(200).send(rows);
-      }
-    })
-  })
-
-
-  //search all users
-app.get('/search',(req, res)=> {
-  const fname = req.query.searchTerm;
-  console.log(fname);
-  db.query(`SELECT * FROM studentdetails WHERE firstname='${fname}'`, (err,rows)=> {
-    if (err) {
-     console.log(err);
-    } else {
-      res.status(200).send(rows);
-      console.log(rows);
-    }
-  })
-})
-
-app.delete('/delete', (req, res) => {
-  const {searchTerm} = req.query;
-  db.query(`DELETE FROM studentdetails WHERE firstname= '${searchTerm}'`, (err, result) => {
+app.get("/", (req, res) => {
+  db.query("SELECT * FROM studentdetails", (err, rows) => {
     if (err) {
       console.log(err);
-      res.status(500).send("Error deleting search term from the database.");
     } else {
-      console.log(`Search term ${searchTerm} deleted successfully.`);
-      res.sendStatus(200);
+      res.status(200).send(rows);
     }
   });
 });
 
+//search all users
+app.get("/search", (req, res) => {
+  const fname = req.query.searchTerm;
+  console.log(fname);
+  db.query(
+    `SELECT * FROM studentdetails WHERE firstname='${fname}'`,
+    (err, rows) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).send(rows);
+        console.log(rows);
+      }
+    }
+  );
+});
+
+app.delete("/delete", (req, res) => {
+  const { searchTerm } = req.query;
+  db.query(
+    `DELETE FROM studentdetails WHERE firstname= '${searchTerm}'`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error deleting search term from the database.");
+      } else {
+        console.log(`Search term ${searchTerm} deleted successfully.`);
+        res.sendStatus(200);
+      }
+    }
+  );
+});
+
+app.post("/update", (req, res) => {
+  try {
+    const { id, firstname, lastname, age, dob, countries, gender } = req.body;
+
+    db.query(
+      `UPDATE studentdetails 
+      SET firstname='${firstname}', lastname='${lastname}', age='${age}', dob='${dob}',countries='${countries}',gender='${gender}'
+      WHERE id = ${id}
+      `,
+      [id, firstname, lastname, age, dob, countries, gender],
+      function (error) {
+        if (error) console.log(error);
+
+        res.status(200).send({ message: "Data updated successfully!" });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 app.listen(3000, () => {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
